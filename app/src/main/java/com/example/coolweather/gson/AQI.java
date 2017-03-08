9437e8b8be4bb522a647a0ade246a7b5c0f3a81b
patrickpan198528 <540952544@qq.com>
2017-03-08 13:43:23 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/3/7.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
